82877586156c0920c4a8912f143533acb4a50699
codeagles <hcn0521@outlook.com>
2020-06-20 16:41:06 +0800

    mapper开启

--- src/main/java/com/base/demo/DemoApplication.java
+++ src/main/java/com/base/demo/DemoApplication.java
@@ -2,8 +2,10 @@ package com.base.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.base.demo.mapper")
 public class DemoApplication {
 
     public static void main(String[] args) {
